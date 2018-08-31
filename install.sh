63f55b2a6236f3f643da76d33f9842b16fdbf419
Michael Graczyk <michael@mgraczyk.com>
2018-08-31 15:14:56 -0700

    Automatically run bittrex scraper

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 . setup.sh
 aws --version
 if [ "$(uname)" == "Darwin" ]; then
-  brew install zstd getsentry/tools/sentry-cli tmux
+  brew install --upgrade zstd tmux
+  brew install getsentry/tools/sentry-cli || brew upgrade getsentry/tools/sentry-cli
 else
   sudo yum install -y zstd tmux git python3 python3-devel
   sudo yum groupinstall "Development Tools"
--- start_all.sh
+++ start_all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 . install.sh
+session_name="$(basename "$(pwd)")"
 ret=0
-tmux -2 attach-session -d || ret=$?
+tmux -2 attach-session -d -t "$session_name" || ret=$?
 if (( $ret != 0 )); then
-  tmux new-session -s "$(basename "$(pwd)")" -d
+  tmux new-session -s "$session_name" -d
   tmux split-window -v './run_forever.sh ./scrape_ws_coinbase'
   tmux split-window -h './run_forever.sh ./compress'
   tmux split-window -v './run_forever.sh ./upload'
-  tmux -2 attach-session -d
+  tmux new-window './run_forever.sh ./scrape_ws_bittrex'
+  tmux -2 attach-session -d -t "$session_name"
 fi
 tmux source .tmux.conf
