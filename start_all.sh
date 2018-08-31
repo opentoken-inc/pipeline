b9075b0b2d9d0713deb617d8f3f530aeb865f953
Michael Graczyk <michael@mgraczyk.com>
2018-08-31 15:02:53 -0700

    Add recovery

--- start_all.sh
+++ start_all.sh
@@ -4,7 +4,7 @@ set -e
 ret=0
 tmux -2 attach-session -d || ret=$?
 if (( $ret != 0 )); then
-  tmux new-session -s "pipeline" -d
+  tmux new-session -s "$(basename "$(pwd)")" -d
   tmux split-window -v './run_forever.sh ./scrape_ws_coinbase'
   tmux split-window -h './run_forever.sh ./compress'
   tmux split-window -v './run_forever.sh ./upload'
