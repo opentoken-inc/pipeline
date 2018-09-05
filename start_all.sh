640eada3d33afabc12e263ff09389a90f1efdd2d
Michael Graczyk <michael@mgraczyk.com>
2018-09-04 19:37:43 -0700

    start scraping huobi

--- start_all.sh
+++ start_all.sh
@@ -6,11 +6,12 @@ ret=0
 tmux -2 attach-session -d -t "$session_name" || ret=$?
 if (( $ret != 0 )); then
   tmux new-session -s "$session_name" -d
-  tmux split-window -v './run_forever.sh ./scrape_ws_coinbase'
   tmux split-window -h './run_forever.sh ./compress'
   tmux split-window -v './run_forever.sh ./upload'
+  tmux new-window './run_forever.sh ./scrape_ws_coinbase'
   tmux new-window './run_forever.sh ./scrape_ws_bittrex'
   tmux new-window './run_forever.sh ./scrape_ws_binance'
+  tmux new-window './run_forever.sh ./scrape_ws_huobi'
   tmux -2 attach-session -d -t "$session_name"
 fi
 tmux source .tmux.conf
